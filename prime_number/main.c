f98f93349532f2f7ef2405a4795bcc9cd7826f8a
SE0NGH0 <ericsungho@naver.com>
2025-02-20 09:43:44 +0900

    프라임넘버 찾기 소스

---
+++ prime_number/main.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int column = 0;
+	int inputValue; // 입력 받은 수
+	int stepValue; // 단계별 검증할 수
+	int incValue; // 프라임넘버를 확인하기 위한 나눔수
+
+	printf("2 이상의 정수를 입력하세요 : ");
+	scanf("%d", &inputValue);
+
+	for (stepValue = 2; stepValue <= inputValue; stepValue++)
+	{
+		for (incValue = 2; incValue < stepValue; incValue++)
+		{
+			if (stepValue % incValue == 0)
+			{
+				break;
+			}
+		}
+		if (incValue == stepValue)
+		{
+			printf("%5d", stepValue);
+			column++;
+			if (column == 5)
+			{
+				column = 0;
+				printf("\n");
+			}
+		}
+	}
+}
